8cde9aae3b2cbed6d00b4fced8a95c4de16f4a47
JinLin Simon Cen <56233967+simoncen@users.noreply.github.com>
2021-07-28 12:37:40 +0800

    Add files via upload

---
+++ OpenData2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# make sure to install these packages before running:
+# pip install pandas
+# pip install sodapy
+
+import pandas as pd
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata("data.cityofnewyork.us", None)
+
+# Example authenticated client (needed for non-public datasets):
+# client = Socrata(data.cityofnewyork.us,
+#                  MyAppToken,
+#                  userame="user@example.com",
+#                  password="AFakePassword")
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("h9gi-nx95", limit=15000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+results_df.to_excel("monitorVC.xlsx")
